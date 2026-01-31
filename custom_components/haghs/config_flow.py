33ff25cd14e8e54aea1985590db2b480ed93d2fc
denzo_91 <deniz.naegeli@gmail.com>
2026-01-31 23:02:39 +0100

    Update config_flow.py v2.2

--- custom_components/haghs/config_flow.py
+++ custom_components/haghs/config_flow.py
@@ -10,10 +10,10 @@ from .const import (
     CONF_RAM_SENSOR,
     CONF_DISK_SENSOR,
     CONF_DB_SENSOR,
-    CONF_LOG_SENSOR,
     CONF_CORE_UPDATE_ENTITY,
     CONF_IGNORE_LABEL,
-    DEFAULT_IGNORE_LABEL,
+    CONF_UPDATE_INTERVAL,
+    DEFAULT_UPDATE_INTERVAL,
 )
 
 class HaghsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -24,13 +24,12 @@ class HaghsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            # Create the integration entry
-            return self.async_create_entry(title="Global Health Score", data=user_input)
+            return self.async_create_entry(title="HAGHS Advisor", data=user_input)
 
-        # The Form Schema (UI)
-        schema = vol.Schema({
+        # Definition der Felder im Setup-Dialog
+        data_schema = vol.Schema({
             vol.Required(CONF_CPU_SENSOR): selector.EntitySelector(
-                selector.EntitySelectorConfig(domain="sensor")
+                selector.EntitySelectorConfig(domain="sensor", device_class="power_factor") # Oder passend
             ),
             vol.Required(CONF_RAM_SENSOR): selector.EntitySelector(
                 selector.EntitySelectorConfig(domain="sensor")
@@ -41,13 +40,12 @@ class HaghsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required(CONF_DB_SENSOR): selector.EntitySelector(
                 selector.EntitySelectorConfig(domain="sensor")
             ),
-            vol.Optional(CONF_LOG_SENSOR): selector.EntitySelector(
-                selector.EntitySelectorConfig(domain="sensor")
-            ),
-            vol.Optional(CONF_CORE_UPDATE_ENTITY, default="update.home_assistant_core_update"): selector.EntitySelector(
+            vol.Required(CONF_CORE_UPDATE_ENTITY): selector.EntitySelector(
                 selector.EntitySelectorConfig(domain="update")
             ),
-            vol.Optional(CONF_IGNORE_LABEL, default=DEFAULT_IGNORE_LABEL): selector.TextSelector(),
+            vol.Required(CONF_IGNORE_LABEL): selector.LabelSelector(),
+            # NEU: Auswahl des Intervalls
+            vol.Optional(CONF_UPDATE_INTERVAL, default=DEFAULT_UPDATE_INTERVAL): vol.In([1, 2, 5, 10, 30, 60]),
         })
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(step_id="user", data_schema=data_schema)
