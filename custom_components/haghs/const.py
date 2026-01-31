997cd54856b18b9f589c8d743f5ca1afefc3b60a
denzo_91 <deniz.naegeli@gmail.com>
2026-01-31 23:01:39 +0100

    Update const.py v2.2

--- custom_components/haghs/const.py
+++ custom_components/haghs/const.py
@@ -1,16 +1,17 @@
 """Constants for the HAGHS integration."""
 
 DOMAIN = "haghs"
+DEFAULT_NAME = "HAGHS Advisor"
 
-# Config Keys
+# Technische Keys für die Speicherung
 CONF_CPU_SENSOR = "cpu_sensor"
 CONF_RAM_SENSOR = "ram_sensor"
 CONF_DISK_SENSOR = "disk_sensor"
 CONF_DB_SENSOR = "db_sensor"
-CONF_LOG_SENSOR = "log_sensor"
 CONF_CORE_UPDATE_ENTITY = "core_update_entity"
 CONF_IGNORE_LABEL = "ignore_label"
+# NEU für v2.2:
+CONF_UPDATE_INTERVAL = "update_interval" 
 
-# Defaults
-DEFAULT_NAME = "System: HA - Global Health Score"
-DEFAULT_IGNORE_LABEL = "haghs_ignore"
+# Standardwerte
+DEFAULT_UPDATE_INTERVAL = 5
